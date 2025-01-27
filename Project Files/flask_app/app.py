cf3b22dc38c923eca3ef36fe04611d6e3a852149
shreekar2005 <shreekarmane7@gmail.com>
2025-01-27 16:13:13 +0530

    made small flask app, created web page that take name as input and give trailer link

--- Project Files/flask_app/app.py
+++ Project Files/flask_app/app.py
@@ -1,15 +1,8 @@
-from flask import Flask     
-app = Flask(__name__)   # Flask constructor 
-  
-# A decorator used to tell the application 
-# which URL is associated function 
-@app.route('/')       
-def hello(): 
-    return 'Hello , write your name in the URL after the /'  
+from flask import Flask, render_template
+app = Flask(__name__)
 
-@app.route('/<name>')
-def hello_world(name):     
-    return 'Well done %s' % name 
-  
-if __name__=='__main__':
-    app.run(debug=True)
+@app.route('/')
+def home():
+   return render_template('index.html')
+if __name__ == '__main__':
+   app.run(debug=True)
