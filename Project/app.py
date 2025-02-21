8e97fea25f1ba4d6722c231f2ec85068c299517b
shreekar2005 <shreekarmane7@gmail.com>
2025-02-21 16:08:24 +0530

    adding port to app.py

--- Project/app.py
+++ Project/app.py
@@ -94,4 +94,4 @@ def recommandetions():
     return render_template('recommendations.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=5000)
