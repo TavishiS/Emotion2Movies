3c08ce34c985f9d9f13eb43d9ca5be6a9ade085d
sanyam-kabra <sanyamkabra901@gmail.com>
2025-03-05 15:49:02 +0530

    chaning number of movies out from promt, changing style of pages

--- Project/prompt2movie.py
+++ Project/prompt2movie.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
-myKey="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"
+myKey="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"
 
-def give3movies(prompt):
+def give5movies(prompt):
   client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=myKey,
@@ -16,18 +16,18 @@ def give3movies(prompt):
     messages=[
       {
         "role": "user",
-        "content": f"prompt:'{prompt}' write only list of 3 movies based on this prompt. output formate should be single line with comma separated movie1, movie2, movie3. please don't write anything else."
+        "content": f"prompt:'{prompt}' write only list of 5 movies based on this prompt. output format should be single line with comma separated movie1, movie2, movie3, movie3, movie4, movie5. please don't write anything else."
       }
     ]
   )
   output = completion.choices[0].message.content
   #print(output)
   output=output.split(",")
-  if len(output)>3:
-    output=output[-3:]
+  if len(output)>5:
+    output=output[-5:]
   return output
 
 if __name__ == "__main__":
   prompt="sleep fun jump wrong paper."
-  output=give3movies(prompt=prompt)
+  output=give5movies(prompt=prompt)
   print(output)
