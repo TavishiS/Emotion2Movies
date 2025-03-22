944ddc98bc4d5c6f80e75ead42f2d130845b1aa7
TavishiS <tavishi.srivastava2004@gmail.com>
2025-03-22 19:52:21 +0530

    adding wishlist array

--- Project/userDatabase.py
+++ Project/userDatabase.py
@@ -8,6 +8,11 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["Users"]
 collection = db["credentials"]
 
+collection.update_many(
+    {"wishlist": {"$exists": False}},  # Only update documents that don't have 'wishlist'
+    {"$set": {"wishlist": []}}  # Initialize as an empty array
+)
+
 def display_data():
     i=0
     for user in collection.find():
@@ -17,12 +22,43 @@ def display_data():
         print("\tuser username: ",user['username'])
         print("\tuser email   : ",user['email'])
         print("\tuser password: ",user['password'])
+        print('\tuser wishlist: ',user['wishlist'])
     print("-------------------------------------------------------")
     print("\nTotal users: ",i,"\n")
     print("-------------------------------------------------------")
     # Close the connection
+
+def add_to_wishlist(user_name, movies_list):
+    for movie in movies_list:
+        if not is_movie_in_wishlist(user_name, movie):
+            collection.update_one(
+                {"username": user_name},
+                {"$push": {"wishlist": movie}}        
+        )
+            
+
+def is_movie_in_wishlist(user_name, movie_name):
+    """Check if a movie is already in the user's wishlist."""
+    user = collection.find_one({"username": user_name, "wishlist": movie_name})
+    return user is not None  # Returns True if movie exists, False otherwise
+
+def remove_from_wishlist(user_name, movie_name):
+    if is_movie_in_wishlist(user_name, movie_name):
+        collection.update_one(
+            {"username": user_name},
+            {"$pull": {"wishlist":movie_name}}
+        )
+
+# def clear_wishlist(user_name):
+#     if collection.find_one({"username": user_name}):
+#         collection.update_one({"wishlis})
+
+    
     
 if __name__ == "__main__":
+    add_to_wishlist("tavi", ["bajirao mastani", "Ghost Ship"])
+    remove_from_wishlist("tavi", "Allah hu akbar")
+    # remove_from_wishlist("tavi", "bajirao mastani")
     #collection.delete_one({"username": "shree123"}) #delete a user if found
     #collection.delete_many({}) # to delte all users
     display_data()
